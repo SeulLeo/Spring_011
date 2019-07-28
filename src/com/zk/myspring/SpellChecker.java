e5f28354dbffcfd127b9530bedcfba4142d749fc
SeulLeo <359269746@qq.com>
2019-07-28 14:57:49 +0800

    Initial commit

---
+++ src/com/zk/myspring/SpellChecker.java
@@ -0,0 +1,13 @@
+package com.zk.myspring;
+
+public class SpellChecker {
+
+	public SpellChecker() {
+		System.out.println("Inside SpellChecker constructor");
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void checkSpelling(){
+		System.out.println("Inside checkSpelling");
+	}
+}
